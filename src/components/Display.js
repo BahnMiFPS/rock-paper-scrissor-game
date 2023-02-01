c18cbce3c7c8f007e63baef7ded3e15a3e6054c6
BahnMiFPS <87252277+BahnMiFPS@users.noreply.github.com>
2023-02-01 16:20:31 +1100

    as

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,14 +1,14 @@
-import React from "react";
-import Player from "./Player";
+import React from "react"
+import Player from "./Player"
 
 function Display() {
-  return (
-    <div className="display">
-      <Player avatarUrl="/avatar/user.jpeg" name="User" />
-      <div className="fighter">VS</div>
-      <Player avatarUrl="/avatar/computer.png" name="Computer" />
-    </div>
-  );
+	return (
+		<div className="display">
+			<Player avatarUrl="/avatar/user.jpeg" name="User" />
+			<div className="fighter">VS</div>
+			<Player avatarUrl="/avatar/computer.png" name="Computer" />
+		</div>
+	)
 }
 
-export default Display;
+export default Display
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React from "react"
 
 function Player({ avatarUrl, name }) {
-  return (
-    <div className="user">
-      <span>{name}</span>
-      <img className="avatar" src="Your code here" alt="Your code here" />
-    </div>
-  );
+	return (
+		<div className="user">
+			<span>{name}</span>
+			<img className="avatar" src={avatarUrl} alt={avatarUrl} />
+		</div>
+	)
 }
 
-export default Player;
+export default Player
